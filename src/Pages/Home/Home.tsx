c1b053152b9b623c3bc7f136a967aad4ddcfa984
Matheus Victor <matheusvictor2000@gmail.com>
2023-02-03 00:57:26 -0300

    fixando o loading na home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -11,9 +11,10 @@ const Home = () => {
   return (
     <div className='container'>
       <h2 className='title'>Melhores filmes:</h2>
-      <div className="movies-container">
-        {error && <p>{error}</p> }
+      <div className="loading">
         {loading && <Loading />}
+      </div>
+      <div className="movies-container">
         {movies && movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
         ))}
